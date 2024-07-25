01c9409b30e6070a16cbd6d3d4905a0551c724aa
get-install-DHY <deherdtyaron@gmail.com>
2024-07-25 12:51:18 +0200

    add forget, prune & policy

--- Linux/Linux-backup.sh
+++ Linux/Linux-backup.sh
@@ -1,5 +1,11 @@
-# make back up
-restic -r sftp:Truenas:../Restic/Ubuntu-laptop --verbose --exclude-file=excludes.txt backup --no-scan /home/ydh/
+# Make back up
+restic -r sftp:Truenas:../Restic/Ubuntu-laptop --exclude-file=excludes.txt backup --no-scan /home/ydh/
 
-# check repo
+# Check repo
 restic -r sftp:Truenas:../Restic/Ubuntu-laptop check
+
+# Forget snapshot
+restic -r sftp:Truenas:../Restic/Ubuntu-laptop forget --keep-last 183
+
+# Delete not referenced snapshots
+restic -r sftp:Truenas:../Restic/Ubuntu-laptop prune
