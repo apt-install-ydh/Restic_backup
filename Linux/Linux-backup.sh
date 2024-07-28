d4c9b8acf4d08fade0d04760432b3ebf4febc868
get-install-DHY <deherdtyaron@gmail.com>
2024-07-28 19:41:57 +0200

    fix linux back up script

--- Linux/Linux-backup.sh
+++ Linux/Linux-backup.sh
@@ -1,39 +1,42 @@
-# Variables
+#!/bin/bash
+# Initialize variables
+backup_status=""
+back_log=""
 REPO="sftp:Truenas:../Restic/Ubuntu-laptop"
 EXCLUDE_FILE="excludes.txt"
 BACKUP_DIR="/home/ydh/"
 SMTP_CREDENTIALS_FILE="creds.txt"
-EMAIL_FROM="email.txt"
-EMAIL_TO=""
+EMAIL_FROM="email_from.txt"
+EMAIL_TO="email_to.txt"
+API_Key="API_Key.txt"
+PASSWORD_FILE="repo_password.txt"
 
+# Attempt to create a backup
+backup_status=$(restic -r $REPO --password-file $PASSWORD_FILE --exclude-file $EXCLUDE_FILE backup $BACKUP_DIR 2>&1)
+backup_log="$backup_status"
 
-# Prepare the JSON payload for the SMTP2GO API
-#JSON_PAYLOAD=$(printf '{"to":"%s", "from":"%s", "subject":"%s", "text":"%s"}' "$EMAIL_TO" "$SMTP_USER" "$subject" "$EMAIL_CONTENT")
+# Check if the backup command failed
+if [[ $? -ne 0 ]]; then
+    EMAIL_SUBJECT="Backup failed"
+else
+    EMAIL_SUBJECT="Backup Succeeded"
+fi
+
+# Status about backup
+NEW_ADDED_SIZE=$(echo "$backup_log" | grep -o 'added [0-9]\+' | cut -d' ' -f2)
+
+# Adjusted to handle array output from restic
+SNAPSHOT_COUNT=$(restic -r $REPO snapshots --password-file $PASSWORD_FILE --json | jq '.[] | .id' | wc -l)
+
+# Generate email body
+EMAIL_BODY="Backup Log:\n$backup_log\n\nNew Added Size: $NEW_ADDED_SIZE bytes\nSnapshot Count: $SNAPSHOT_COUNT"
+
+JSON_PAYLOAD=$(printf '{"sender":"%s", "to": ["%s"], "subject": "%s", "text_body": "%s"}' "$(cat $EMAIL_FROM)" "$(cat $EMAIL_TO)" "$EMAIL_SUBJECT" "$EMAIL_BODY" | sed ':a;N;$!ba;s/\n/\\n/g')
 
 # Send the email using SMTP2GO API
 curl --request POST \
      --url https://api.smtp2go.com/v3/email/send \
      --header 'Content-Type: application/json' \
-     --header 'X-ssmtp2go-Api: api-key-here' \
+     --header "X-Smtp2go-Api-Key: $(cat $API_Key)" \
      --header 'accept: application/json' \
      --data "$JSON_PAYLOAD"
-
-
-# Check for errors in the SMTP2GO response
-if [[ $? -ne 0 ]]; then
-    echo "Error sending email via SMTP2GO"
-    exit 1
-fi
-
-# Make back up
-restic -r $REPO --exclude-file=excludes.txt backup $BACKUP_DIR /home/ydh/ || { send_email "Backup Failed" "Backup failed."; exit 1; }
-
-
-# Check repo
-restic -r $REPO check || { send_email "Repository Check Failed" "Repository check failed."; exit 1; }
-
-# Forget snapshot
-restic -r $REPO forget --keep-last 183 || { send_email "Snapshot Management Failed" "Failed to manage snapshots."; exit 1; }
-
-# Delete not referenced snapshots
-restic -r $REPO prune || { send_email "Pruning Failed" "Failed to prune unreferenced snapshots."; exit 1; }
